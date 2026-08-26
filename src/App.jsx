c13f40f999474f5dd3bcf14f6b843b5b83f192f6
Lucky Kumari <kumarilucky01437@gmail.com>
2026-08-26 22:42:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const profile = {
   github: 'https://github.com/luckylucky110507',
   instagram: 'https://www.instagram.com/k11_lucky',
   location: 'Delhi/NCR, India',
-  resumeUrl: '/Resume Lucky..pdf',
+  resumeUrl: '/resume.pdf',
   photo: '/profile.jpeg',
 }
 
@@ -1152,3 +1152,4 @@ export default function App() {
     </>
   )
 }
+
